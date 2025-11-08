93ee01b63194e465f1ef679c061b2ab11f688431
riyasrihan76-svg <riyasrihan76@gmail.com>
2025-11-08 14:48:43 +0400

    Add files via upload

---
+++ 03 - Biography.py
@@ -0,0 +1,7 @@
+biography = {
+    "Name": "Rihan Riyas",
+    "Hometown": "Dubai",
+    "Age": 19
+}
+
+print("Name: " + biography["Name"] + "\nHometown: " + biography["Hometown"] + "\nAge: " + str(biography["Age"]))
