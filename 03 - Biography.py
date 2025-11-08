36f36bc8a1e81e876ba2ab850d3bbba5314a85d9
riyasrihan76-svg <riyasrihan76@gmail.com>
2025-11-08 14:47:38 +0400

    Delete 03 - Biography.py

--- 03 - Biography.py
+++
@@ -1,7 +0,0 @@
-biography = {
-    "Name": "Ahsan Haris",
-    "Hometown": "Dubai",
-    "Age": 19
-}
-
-print("Name: " + biography["Name"] + "\nHometown: " + biography["Hometown"] + "\nAge: " + str(biography["Age"]))
